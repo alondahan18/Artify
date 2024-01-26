9c22fec5f66e14be266abe464dce2fbd6ebacc12
alondahan18 <alond2207@gmail.com>
2024-01-26 22:46:49 +0200

    slow progress

--- artify/src/Scoreboard/Scoreboard.js
+++ artify/src/Scoreboard/Scoreboard.js
@@ -1,6 +1,32 @@
 import './Scoreboard.css'
+import React from 'react';
 
+const highScores = [
+    { name: 'Bill', score: 123 },
+    { name: 'Garry', score: 34 },
+    // ... other player data
+  ];
 function Scoreboard() {
-
+    return (
+        <div>
+            <span><h1>Artify</h1><span>Home</span></span>
+        <div className="wrapper">
+          <table>
+            <caption>High Scores (total {highScores.length})</caption>
+            <tr>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+            {highScores.map((player, index) => (
+              <tr key={index}>
+                <td>{player.name}</td>
+                <td>{player.score}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+        </div>
+      );
+    
 }
 export default Scoreboard
